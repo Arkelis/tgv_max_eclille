c1cbfd52a5cfd2517ddf2fd7d455113bd035a3be
Lyderic Dutillieux <lyderic.dutillieux@centrale.centralelille.fr>
2018-01-16 15:16:43 +0100

    Implemantation d'une sécurité pour les 30 jours

--- main.py
+++ main.py
@@ -4,16 +4,28 @@ from gen_url import *
 from time import sleep
 
 
-recherche_par_defaut = ("Lille","Paris",date.today(),True)
+recherche_par_defaut = ("Lille","Paris",date.today()+deltatime(30),True)
 recherche= recherche_par_defaut
+
 
 toaster = ToastNotifier()
 #toaster.show_toast("Salut",
 #              "Salut Ca va mec!!",duration=10)
 
 browser = Browser(driver_name='chrome',**{'executable_path':'C:\Program Files (x86)\WebDriver\chromedriver.exe','headless' : False})
+
+
 if( bool(int(input("Modifier recherche par défaut ? (0/1) : ")))):
-    config = demander_args()
+    recherche = demander_args()
+
+#Tant qu'on n'a pas obtenu une date où des TGV MAX ont été publiés
+#On redemande un nouveau trajet
+while(recherche[rech['JDEP']] > date.today()+timedelta(30)):
+    print("\nERREUR : les TGV Max sont publiés seulement 30 jours à l'avance !")
+    print("Essayez une autre recherche !\n")
+    if( bool(int(input("Modifier recherche par défaut ? (0/1) : ")))):
+        recherche = demander_args()
+    
 
 browser.visit(gen_url(*recherche))
     
