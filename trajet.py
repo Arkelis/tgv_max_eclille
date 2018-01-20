7512e2e9d486c64eb6a54471e006cb9d3aa8e715
Arkelis <35486980+Arkelis@users.noreply.github.com>
2018-01-20 10:14:35 +0100

    Syntaxe

--- trajet.py
+++ trajet.py
@@ -7,27 +7,26 @@ class Trajet():
     
 
     def __init__(self,id_sncf=None ,id_bdd=None,gdep=None,garr=None,hdep=None,date=None,dur = None,option=None,nb=None):
-        
-        id_train_sncf = id_sncf
-        id_tj_bdd; = id_bdd
-        g_dep= gdep
-        g_arr = garr
-        d_dep = d
-        h_dep = hdep
-        duree = dur
-        option_trajet= option
-        nb_places =nb
+        self.id_train_sncf = id_sncf
+        self.id_tj_bdd; = id_bdd
+        self.g_dep= gdep
+        self.g_arr = garr
+        self.d_dep = d
+        self.h_dep = hdep
+        self.duree = dur
+        self.option_trajet= option
+        self.nb_places =nb
     
     @staticmethod
-    def lire_page(browser):
+    def lire_page(self, browser):
         liste_traj = []    
         traj_lu = Trajet()
 
     def __str__(self):
-        print("Gare de départ : " + g_dep)
-        print("Gare d'arrivée : " + g_arr)
-        print("Heure de départ :" + h_dep)
-        print("Durée de trajet :" + duree)
+        print("Gare de dÃ©part : " + g_dep)
+        print("Gare d'arrivÃ©e : " + g_arr)
+        print("Heure de dÃ©part :" + h_dep)
+        print("DurÃ©e de trajet :" + duree)
         print("Option de trajets :" + option_trajet)
         print("Nb de places dispo :" +nb_places)
     
